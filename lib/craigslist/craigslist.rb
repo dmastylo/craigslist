923f5fa452695b07e0d943daf9e0b0530be1849f
Damian Mastylo <dmastylo@codequarry.net>
2013-01-27 21:33:12 -0500

    Begin process of modularizing code (breaking up code into many readable methods)

--- lib/craigslist/craigslist.rb
+++ lib/craigslist/craigslist.rb
@@ -80,42 +80,7 @@ module Craigslist
       for i in 0..(max_results / 100)
         uri = self.more_results(uri, i) if i > 0
         doc = Nokogiri::HTML(open(uri))
-
-        doc.xpath("//p[@class = 'row']").each do |node|
-          search_result = {}
-
-          inner = Nokogiri::HTML(node.to_s)
-          inner.xpath("//a").each_with_index do |inner_node, index|
-            if index.even?
-              search_result['text'] = inner_node.text.strip
-              search_result['href'] = inner_node['href']
-            end
-          end
-
-          inner.xpath("//span[@class = 'itempp']").each do |inner_node|
-            search_result['price'] = inner_node.text.strip
-          end
-
-          inner.xpath("//span[@class = 'itempn']/font").each do |inner_node|
-            search_result['location'] = inner_node.text.strip[1..(inner_node.text.strip.length - 2)].strip
-          end
-
-          if Craigslist::PERSISTENT.images
-            inner.xpath("//span[@class = 'itempx']/span[@class = 'p']").each do |inner_node|
-              if inner_node.text.include?('img') || inner_node.text.include?('pic')
-                search_result['has_image'] = true
-                search_result['images'] = []
-                doc_post = Nokogiri::HTML(open(search_result['href']))
-                doc_post.css('div#thumbs a').each do |img|
-                  search_result['images'] << img['href']
-                end
-              end
-            end
-          end
-
-          search_results << search_result
-          break if search_results.length == max_results
-        end
+        scrape_results(search_results, max_results, doc)
       end
 
       search_results
@@ -129,5 +94,43 @@ module Craigslist
 
   def self.more_results(uri, result_count=0)
     uri + "index#{result_count.to_i * 100}.html"
+  end
+
+  def self.scrape_results(search_results, max_results, doc)
+    doc.xpath("//p[@class = 'row']").each do |node|
+      search_result = {}
+
+      inner = Nokogiri::HTML(node.to_s)
+      inner.xpath("//a").each_with_index do |inner_node, index|
+        if index.even?
+          search_result['text'] = inner_node.text.strip
+          search_result['href'] = inner_node['href']
+        end
+      end
+
+      inner.xpath("//span[@class = 'itempp']").each do |inner_node|
+        search_result['price'] = inner_node.text.strip
+      end
+
+      inner.xpath("//span[@class = 'itempn']/font").each do |inner_node|
+        search_result['location'] = inner_node.text.strip[1..(inner_node.text.strip.length - 2)].strip
+      end
+
+      if Craigslist::PERSISTENT.images
+        inner.xpath("//span[@class = 'itempx']/span[@class = 'p']").each do |inner_node|
+          if inner_node.text.include?('img') || inner_node.text.include?('pic')
+            search_result['has_image'] = true
+            search_result['images'] = []
+            doc_post = Nokogiri::HTML(open(search_result['href']))
+            doc_post.css('div#thumbs a').each do |img|
+              search_result['images'] << img['href']
+            end
+          end
+        end
+      end
+
+      search_results << search_result
+      break if search_results.length == max_results
+    end
   end
 end
